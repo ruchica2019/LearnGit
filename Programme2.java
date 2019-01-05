b7a8cd5f5fff590810f2abef4ad506cb6aa92790
ruchika <ruchika.kapoor@lntinfotech.com>
2019-01-06 00:24:40 +0530

    changes committed

--- Programme2.java
+++ Programme2.java
@@ -1,10 +1,6 @@
 public class Programme2{
 	public static void main(String args[]){
 		System.out.println("Second change done");
-		System.out.println("Second change done");
-		System.out.println("This is my programme");
-		System.out.println("Hello");
-		System.out.println("Hello");
 	}
 
 }
