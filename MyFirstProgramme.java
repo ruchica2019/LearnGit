fa1b530b33eb128a4e6aa344343ef4fe7359ae9d
ruchika <ruchika.kapoor@lntinfotech.com>
2019-01-06 19:52:51 +0530

    Changes to be merged to feature branch

--- MyFirstProgramme.java
+++ MyFirstProgramme.java
@@ -1,4 +1,6 @@
 public class MyFirstProgramme{
+
+	private Integer intVal = 100;
 	private String myVariable = "abc";
 	private String myVariable = null;
 }
--- Programme2.java
+++ Programme2.java
@@ -1,5 +1,8 @@
 public class Programme2{
 	public static void main(String args[]){
+		/**
+		 * Added comments
+		 * */
 		System.out.println("Second change done");
 	}
 
